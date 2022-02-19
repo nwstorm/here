f787344bb1300ce9f67ca30edd04d8b1ae96f71d
anne <anneguo3@gmail.com>
2022-02-19 00:55:17 -0800

    Added basic home page with stand-in data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { Route } from 'wouter';
-import logo from './logo.svg';
-import './App.css';
-import Testing from './pages/Testing';
+import { Route } from "wouter";
+import logo from "./logo.svg";
+import "./App.css";
+import Testing from "./pages/Testing";
+import Home from "./pages/Home";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   const getLocation = () => {
-    if(!navigator.geolocation) {
-      console.log('Geolocation is not supported by your browser');
+    if (!navigator.geolocation) {
+      console.log("Geolocation is not supported by your browser");
     } else {
-      console.log('Locating…');
+      console.log("Locating…");
       navigator.geolocation.watchPosition(
         (success) => console.log(success),
         (error) => console.log(error)
       );
     }
-  }
+  };
   return (
-    <>
+    <ChakraProvider>
       <Route path="/testing" component={Testing} />
       <Route path="/waho" component={Testing} />
-    </>
+      <Route path="/home" component={Home} />{" "}
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/PostsList.js
@@ -0,0 +1,19 @@
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from "@chakra-ui/react";
+
+export default function PostsList() {
+  console.log("hit here");
+  return (
+    <Stat>
+      <StatLabel>Collected Fees</StatLabel>
+      <StatNumber>£0.00</StatNumber>
+      <StatHelpText>Feb 12 - Feb 28</StatHelpText>
+    </Stat>
+  );
+}
