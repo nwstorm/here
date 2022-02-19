bd02e9b43f938f877ac4aa402b89119003f1c1f1
anne <anneguo3@gmail.com>
2022-02-19 01:22:56 -0800

    added initial posts lists format

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -7,13 +7,20 @@ import {
   StatGroup,
 } from "@chakra-ui/react";
 
-export default function PostsList() {
-  console.log("hit here");
-  return (
-    <Stat>
-      <StatLabel>Collected Fees</StatLabel>
-      <StatNumber>£0.00</StatNumber>
-      <StatHelpText>Feb 12 - Feb 28</StatHelpText>
-    </Stat>
-  );
+export default function PostsList(props) {
+  const postsList = [
+    { title: "abc", body: "123" },
+    { title: "def", body: "456" },
+  ];
+
+  const listItems = postsList.map((post) => {
+    return (
+      <Stat>
+        <StatLabel>{post.title}</StatLabel>
+        <StatHelpText>{post.body}</StatHelpText>
+      </Stat>
+    );
+  });
+
+  return listItems;
 }
