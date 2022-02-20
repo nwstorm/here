d607519e1077ff135a6d5ef4a4d48c7dc174d26f
Victoriakim <victoriakim048@gmail.com>
2022-02-20 02:22:49 -0800

    This is the better Loading Page version :)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Route } from "wouter";
 import "./App.css";
 import Home from "./pages/Home";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { AuthContext } from "./utils/auth";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
@@ -13,19 +13,12 @@ setPersistence(auth, browserLocalPersistence)
   .then(res => console.log(res))
   .catch(err => console.log(err))
 
-const theme = extendTheme({
-  fonts: {
-    heading: 'HK Grotesk, sans-serif',
-    body: 'HK Grotesk, sans-serif',
-  },
-})
-
 function App() {
   const [user, setUser] = useState(null)
   const value = { auth, user, setUser }
 
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <AuthContext.Provider value={value}>
         <Route path="/" component={Home} />
       </AuthContext.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import PostsList from "../components/PostsList";
 import CreatePostModal from "../components/CreatePostModal";
 import { Box, Container, IconButton, useDisclosure, Text, VStack, Stack} from "@chakra-ui/react";
 import TagsModal from "../components/TagsModal";
-import React from "react";
+import {React} from "react";
 
 export default function Home() {
   const [location, setLocation] = useState([]);
@@ -17,15 +17,51 @@ export default function Home() {
     onClose: onCloseSettings,
   } = useDisclosure();
 
+  // const [loadingscreen] =
+  
+
+  // const [mainscreen] = 
+  // <> 
+  // <Container maxWidth="90%">
+  //   {location && (
+  //     <Stack direction={["column", "row"]} spacing="24px">
+  //       <p>
+  //         Your location is {location.latitude}, {location.longitude}{" "}
+  //         (accuracy of {location.accuracy})
+  //       </p>
+  //       <IconButton
+  //         isRound
+  //         icon={<FontAwesomeIcon icon={faUserGear} />}
+  //         onClick={onOpenSettings}
+  //       />
+  //     </Stack>
+  //   )}
+  //   {location && (
+  //     <Box color="black">
+  //       <PostsList location={location} />
+  //     </Box>
+  //   )}
+  // </Container>
+  // <IconButton
+  //   isRound
+  //   size="lg"
+  //   pos="fixed"
+  //   right="2em"
+  //   bottom="2em"
+  //   icon={<FontAwesomeIcon icon={faPlus} />}
+  //   onClick={onOpen}
+  // />
+  // </>
+
   // Initialize geolocation tracking
   useEffect(() => {
     if (!navigator.geolocation) {
       console.log("Geolocation is not supported by your browser");
     } else {
       console.log("Locating…");
-      setLoading()
-      navigator.geolocation.watchPosition(
+        navigator.geolocation.watchPosition(
         (success) => {
+          setLoading(false)
           console.log(
             `Your location is ${success.coords.latitude}, ${success.coords.longitude} (accuracy of ${success.coords.accuracy})`
           );
@@ -36,45 +72,47 @@ export default function Home() {
     }
   }, []);
 
-  return (
-    <>
-      <Container maxWidth="90%">
-        {location && (
-          <Stack direction={["column", "row"]} spacing="24px">
-            <p>
-              Your location is {location.latitude}, {location.longitude}{" "}
-              (accuracy of {location.accuracy})
-            </p>
-            <IconButton
-              isRound
-              icon={<FontAwesomeIcon icon={faUserGear} />}
-              onClick={onOpenSettings}
-            />
-          </Stack>
-        )}
-        {location && (
-          <Box color="black">
-            <PostsList location={location} />
-          </Box>
-        )}
-      </Container>
-      <IconButton
-        isRound
-        size="lg"
-        pos="fixed"
-        right="2em"
-        bottom="2em"
-        icon={<FontAwesomeIcon icon={faPlus} />}
-        onClick={onOpen}
-      />
-      <CreatePostModal location={location} isOpen={isOpen} onClose={onClose} />
+return ( <>
+  <CreatePostModal location={location} isOpen={isOpen} onClose={onClose} />
+  <TagsModal isOpen={isOpenSettings} onClose={onCloseSettings} />{" "}]
 
-      <VStack loading={loading} spacing={100} align='center'>
-        <Text fontSize='32px' color="#319795">Glad ur here!</Text>
-        <Box bg='tomato' h='75%' w='75%' p={10}>Insert animation here</Box>
-        <Text fontSize='18px' color="#319795" >Collecting Acorns...</Text>
-      </VStack>
-      <TagsModal isOpen={isOpenSettings} onClose={onCloseSettings} />{" "}
-    </>
-  );
+  {loading? <>
+   <VStack loading={loading} spacing={100} align='center'>
+    <Text fontSize='32px' color="#319795">Glad ur here!</Text>
+    <Box bg='tomato' h='75%' w='75%' p={10}>Insert animation here</Box>
+    <Text fontSize='18px' color="#319795" >Collecting Acorns...</Text>
+    </VStack>
+  </> : <> 
+  <Container maxWidth="90%">
+    {location && (
+      <Stack direction={["column", "row"]} spacing="24px">
+        <p>
+          Your location is {location.latitude}, {location.longitude}{" "}
+          (accuracy of {location.accuracy})
+        </p>
+        <IconButton
+          isRound
+          icon={<FontAwesomeIcon icon={faUserGear} />}
+          onClick={onOpenSettings}
+        />
+      </Stack>
+    )}
+    {location && (
+      <Box color="black">
+        <PostsList location={location} />
+      </Box>
+    )}
+  </Container>
+  <IconButton
+    isRound
+    size="lg"
+    pos="fixed"
+    right="2em"
+    bottom="2em"
+    icon={<FontAwesomeIcon icon={faPlus} />}
+    onClick={onOpen}
+  />
+  </>}
+  </>
+    );
 }
