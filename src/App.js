edbeae709e74fb9bc5da228908b7579ca4255f78
Kevin Zou <kevin.zp.zou@gmail.com>
2022-02-20 00:36:13 -0800

    Add and set HK Grotesk as font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Route } from "wouter";
 import "./App.css";
 import Home from "./pages/Home";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'HK Grotesk, sans-serif',
+    body: 'HK Grotesk, sans-serif',
+  },
+})
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Route path="/" component={Home} />
     </ChakraProvider>
   );
